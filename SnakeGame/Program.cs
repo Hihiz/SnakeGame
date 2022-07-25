bb4ba5d783231d7fa9163e9d23fe6cf950933e96
Hihiz <public@mail.ru>
2022-07-25 21:22:35 +0300

    Сделать размер окна

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using static System.Console;
 
 namespace SnakeGame
 {
     class Program
     {
+        private const int MapWidth = 30;
+        private const int MapHeight = 20;
+
         static void Main(string[] args)
         {
+            SetWindowSize(MapWidth, MapHeight);
+            SetBufferSize(MapWidth, MapHeight);
+            CursorVisible = false;
 
+            ReadKey();
         }
     }
 }
